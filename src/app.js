2257979cbe06e2aa4e129b54be9e30482ad0b987
TonyMM55 <tonymmoran@gmail.com>
2023-02-22 21:23:53 +0000

    completed so far as a group

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // In src/app.js add this code:
 const express = require('express');
+const { Cat } = require('./models');
 
 const app = express();
 
@@ -8,5 +9,25 @@ const app = express();
 app.use(express.json());
 
 // we will put our routes and controller functions here
+
+app.post('/cats',async (req, res) => {
+const cat = await Cat.create(req.body)
+  res.status(201).json(cat);
+});
+
+app.get('/cats',async (req, res) => {
+    const cat = await Cat.findAll({ where: req.query })
+      res.status(200).json(cat);
+    });
+
+app.get('/cats/:catId',async (req, res) => {
+    const cat = await Cat.findByPk(req.params.catId)
+        res.status(200).json(cat);
+    });
+
+app.patch('/cats/:catId',async (req, res) => {
+    const cat = await Cat.update(req.body, { where: { id: req.params.catId } })
+        res.status(200).json(cat);
+    });
 
 module.exports = app;
